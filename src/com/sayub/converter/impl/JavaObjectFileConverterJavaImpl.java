b40fd4b26e14ee18cb45eb0bb2f6aa945aeadd5b
Manjit Shakya <mnzitshakya@gmail.com>
2025-02-05 21:28:05 +0545

    feat: uniform

--- src/com/sayub/converter/impl/JavaObjectFileConverterJavaImpl.java
+++ src/com/sayub/converter/impl/JavaObjectFileConverterJavaImpl.java
@@ -2,6 +2,7 @@ package com.sayub.converter.impl;
 
 import com.sayub.converter.JavaObjectFileConverter;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -13,31 +14,50 @@ public class JavaObjectFileConverterJavaImpl<T> implements JavaObjectFileConvert
 
     @Override
     public void serialize(T object, String fileName) {
-        // Write data to file
-        try (FileOutputStream fos = new FileOutputStream(fileName + FILE_SUFFIX)) {
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(object);
-            oos.close();
-            fos.close();
+
+        File file = new File(fileName + FILE_SUFFIX);
+
+        try {
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(object);
+            objectOutputStream.close();
+            
+            fileOutputStream.close();
+
         } catch (Exception e) {
-            System.out.println("File not found: " + fileName);
+            System.out.println("Exception: " + e.getMessage());
         }
+
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public T deserialize(String fileName) {
-        try (FileInputStream fileInputStream = new FileInputStream(fileName + FILE_SUFFIX)) {
+
+        try {
+
+            FileInputStream fileInputStream = new FileInputStream(fileName + FILE_SUFFIX);
+
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             T object = (T) objectInputStream.readObject();
 
             objectInputStream.close();
+
             fileInputStream.close();
 
             return object;
+
         } catch (Exception e) {
-            System.out.println("File not found: " + fileName);
+            System.out.println("Exception: " + e.getMessage());
             return null;
         }
+
     }
 }
