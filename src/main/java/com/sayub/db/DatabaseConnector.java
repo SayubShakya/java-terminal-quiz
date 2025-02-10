846ffb02188de403c7f790237300acaae73b3c6d
Sayub <shakya.sayub123@gmail.com>
2025-02-10 13:32:56 +0545

    added initial files

--- src/main/java/com/sayub/db/DatabaseConnector.java
+++ src/main/java/com/sayub/db/DatabaseConnector.java
@@ -1,19 +1,16 @@
-package com.sayub.util;
+package com.sayub.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnectionUtil {
+public class DatabaseConnector {
 
     private static final String URL = "jdbc:mysql://localhost:3306/terminal_quiz_application";
     private static final String USER = "root";
-    private static final String PASSWORD = "Root@12345";
+    private static final String PASSWORD = "9828807288";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
-
-
-
